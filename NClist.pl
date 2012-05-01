fca20719ab3e88f1443c16ba05b4029164e102eb
Yogesh Pandit <yogeshp08@gmail.com>
2012-05-01 18:34:07 -0500

    NClist algorithm in Perl

---
+++ NClist.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl 
+#===============================================================================
+#
+#         FILE: NClist.pl
+#
+#        USAGE: ./NClist.pl  
+#
+#  DESCRIPTION: Nested Containment list (NClist) is a interval database representation.
+#  				O(n + log N); N = database size, n = result set
+#
+#      OPTIONS: ---
+#        NOTES: This is a re-implementation for practice.
+#       AUTHOR: Yogesh Pandit 
+#      COMPANY: dictyBase.org
+#      CREATED: 04/27/2012 02:27:18
+#===============================================================================
+
+use strict;
+use warnings;
+
