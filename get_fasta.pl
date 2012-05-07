91b1c83cfa6ee13170649108873e2fed8c52b9dc
Yogesh Pandit <yogeshp08@gmail.com>
2012-05-07 14:57:22 -0500

    RegEx for index ID working

--- get_fasta.pl
+++ get_fasta.pl
@@ -1,50 +1,61 @@
-#!/usr/bin/perl 
+#!/usr/bin/perl
 
 use strict;
 use Pod::Usage;
 use Getopt::Long;
 use Bio::DB::Fasta;
+use Bio::SeqIO;
 
-my ( $fasta_file, $id_list );
+my ( $fasta_file, $id_list, $out_file );
 
 GetOptions(
     'id|id-list=s' => \$id_list,
     'fa|fasta=s'   => \$fasta_file,
+    '-o|output-file=s' => \$out_file,
     'h|help'       => sub { pod2usage() }
 );
 pod2usage() if !$id_list;
 pod2usage() if !$fasta_file;
 
-my $db = build_index($fasta_file);
-write_fasta( $db, $id_list );
+my $seqout;
+if ($out_file ne "") {
+	$seqout = Bio::SeqIO->new( -file => ">$out_file", -format => 'fasta');	
+} else {
+	$seqout = Bio::SeqIO->new( -fh => \*STDOUT, -format => 'fasta');	
+}
+my $db = build_index ( $fasta_file );
+write_fasta( $db, $id_list, $seqout);
 
 sub build_index {
-    my ($fasta_file) = @_;
-    my $inx = Bio::DB::Fasta->new(
-        $fasta_file,
-        -make_id => sub {
-            my ($id) = @_;
-            retrun $1 if $id =~ /^>(DDB\d{1, 10})\|\S+$/;
-        }
-    );
-    return $inx;
+	my ($fasta_file) = @_;
+	my $db = Bio::DB::Fasta->new ( 
+		$fasta_file, 
+		-reindex => 1, 
+		-makeid => sub {
+						my ($ID) = @_;
+						$ID =~ /^>(DDB\d{1,10})\|.*$/;
+						$1;
+					});
+	return $db;
 }
 
-sub get_fasta {
-    my ( $db, $id_list ) = @_;
-    my $file = IO::File->new( $id_list, 'r' ) or die "cannot open file:$!";
-    while ( my $line = $file->getline ) {
-        my $seq = $db->get_Seq_by_id( chomp($line) );
-        # Returns Bio::Seq object
-        # Read the bioperl doc to figure out how to write it to a file
+sub write_fasta {
+    my ( $db, $id_list, $out_file ) = @_;
+    my $f = IO::File->new( $id_list, 'r' ) or die "Cannot open file: $!";
+	while ( my $line = $f->getline ) {
+		chomp($line);
+        my $seqobj = $db->get_Seq_by_id($line);
+		#print $line."\n".$seqobj->seq."\n\n";
+		my $seq = Bio::Seq->new( -id => $line, 
+								 -seq => $seqobj->seq );
+		$seqout->write_seq($seq);
     }
-    $file->close;
+    $f->close;
 }
 
 =head1 NAME
 
 B<get_fasta.pl> - [One line description of application purpose]
-
 
 =head1 SYNOPSIS
 
@@ -52,11 +63,10 @@ perl get_fasta.pl -id idlist.txt -fa dicty.fasta
 
 =head1 REQUIRED ARGUMENTS
 
-B<[-id|--id-list]> - [Write something here]
+B<[-id|--id-list]> - [List of IDs to search for in the indexed database]
 
-B<[-id|--id-list]> - [Write something here]
+B<[-fa|--fasta]> - [Fasta file to be indexed]
 
 =head1 OPTIONS
 
 B<[-h|-help]> - display this documentation.
-
